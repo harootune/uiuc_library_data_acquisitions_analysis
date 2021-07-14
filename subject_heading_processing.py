8acba0564d06d335e8ac42a11db3372ca1ee6955
Harootune <dharootuneotis@gmail.com>
2021-07-14 18:52:29 -0400

    wordcloud generation

--- subject_heading_processing.py
+++ subject_heading_processing.py
@@ -3,6 +3,8 @@ import re
 import collections
 
 import pandas as pd
+import wordcloud as wc
+import matplotlib.pyplot as plt
 
 from call_number_processing import dataframe_from_data_folder
 
@@ -38,6 +40,15 @@ def get_subject_heading_counts(record_df: pd.DataFrame):
     record_df.apply(per_row_heading_count, axis=1)
     
     return collections.Counter(heading_list)
+
+
+def create_wordcloud(heading_list: collections.Counter):
+
+    heading_cloud = wc.WordCloud().generate_from_frequencies(heading_list)
+
+    plt.imshow(heading_cloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
         
 
 def main(result_file_name: str):
@@ -45,15 +56,19 @@ def main(result_file_name: str):
 
     heading_frequency_counter = get_subject_heading_counts(record_df)
 
+    create_wordcloud(heading_frequency_counter)
+
     heading_frequency_df = pd.DataFrame.from_dict(heading_frequency_counter, orient="index")
 
     heading_frequency_df = heading_frequency_df.rename({0: "count"}, axis=1)
 
     heading_frequency_df = heading_frequency_df.sort_values("count", ascending=False)
 
+
     with open(result_file_name, mode="w") as outfile:
         pd.set_option('display.max_rows', None)
         print(heading_frequency_df, file=outfile)
+
 
 if __name__ == "__main__":
     result_file_name = "unique_subject_headings.txt"
